63c72aa2a486ca0c24d72460a981c8d5b9cc1399
Faisal <it13.knitto@gmail.com>
2024-06-05 10:14:00 +0700

    Develop toggle todo feature

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { todoData } from "./data/todosData";
 import Todos from "./components/Todos";
 
 const App = () => {
+  const toggleCompleted = (id: number) => {
+    const updatedTodos = todos.map((todo: IDataTodo) => {
+      if (todo.id === id) todo.completed = !todo.completed;
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   const [todos, setTodos] = useState<IDataTodo[]>(todoData);
   console.log(todos);
   return (
     <div className="text-center p-[12px]">
       <h1 className="text-[36px] font-bold">My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   );
 };
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { ITodo } from "../interface";
 
-const TodoItem: React.FC<ITodo> = ({ todo }) => {
+const TodoItem: React.FC<ITodo> = ({ todo, toggleCompleted }) => {
+  const getTodoTitleStyle = () => {
+    if (todo.completed) return "line-through";
+    else return "";
+  };
+
   return (
-    <div className="border-2 border-sky-200 text-[24px]">
-      <p>{todo.title}</p>
+    <div className="border-2 border-sky-200 text-[24px] flex justify-center items-center">
+      <input
+        type="checkbox"
+        className="mr-[10px] h-[18px] w-[18px]"
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      <p className={getTodoTitleStyle()}>{todo.title}</p>
     </div>
   );
 };

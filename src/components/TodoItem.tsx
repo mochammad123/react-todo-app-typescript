00c3e4b0bb58a111a30b087a0b5672f310d160c1
Faisal <it13.knitto@gmail.com>
2024-06-05 09:28:02 +0700

    Todo 1 add css to components

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { ITodo } from "../interface";
 
 const TodoItem: React.FC<ITodo> = ({ todo }) => {
-  return <p>{todo.title}</p>;
+  return (
+    <div className="border-2 border-sky-200 text-[24px]">
+      <p>{todo.title}</p>
+    </div>
+  );
 };
 
 export default TodoItem;
